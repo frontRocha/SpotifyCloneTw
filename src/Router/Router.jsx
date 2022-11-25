ac015836f1991c4348cbfe81c50bac6e854e00a1
Mauricio Pedro <mauriciopedeo007@gmail.com>
2022-11-25 15:41:04 -0300

    testes

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "../Components/Layout/Footer/Footer.jsx";
-import Header from "../Components/Layout/Header/Header.jsx";
-import Home from "../Pages/Home/Home.jsx"
+import Footer from '../Components/Layout/Footer/Footer'
+import Header from "../Components/Layout/Header/Header";
+import Home from "../Pages/Home/Home"
 
 
 export default function Router() {
