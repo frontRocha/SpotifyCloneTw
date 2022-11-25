aafd6d0c860c403fb4692ad613be783d23ddf604
estudantedehtml <mauriciopedeo007@gmail.com>
2022-11-25 13:38:35 -0300

    ajustes

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "../Components/Layout/Footer/Footer.jsx";
-import Header from "../Components/Layout/Header/header";
+import Header from "../Components/Layout/Header/Header";
 import Home from "../Pages/Home/Home";
 
 export default function Router() {
