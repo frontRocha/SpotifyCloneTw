00574f5362736e648f7d7a2617a468c414210cbf
Mauricio Pedro <mauriciopedeo007@gmail.com>
2022-11-25 15:48:48 -0300

    teste

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,8 +3,6 @@ import Footer from "../Components/Layout/Footer/Footer.jsx";
 import Header from "../Components/Layout/Header/Header.jsx";
 import Teste from "../Pages/teste/Teste.jsx";
 
-
-
 export default function Router() {
     return(
         <BrowserRouter>
